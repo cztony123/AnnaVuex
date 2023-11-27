a037e9d166b156811d9076be14322df6d009b7f2
groot <groot@ndxo.io>
2023-11-27 14:26:35 +0800

    dev

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,25 @@
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 
-// Vue.use(Vuex)
+const routes = [
+  {
+    path: '/index',
+    name: 'index',
+    component: () => import('../views/index.vue')
+  },
+  {
+    path: '/',
+    name: 'pages',
+    component: () => import('../pages/index/index.vue')
+  },
+]
 
-// const routes = [
-//   {
-//     path: '/insex',
-//     name: 'insex',
-//     component:index
-//   },
-// ]
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
 
-// const router = new VueRouter({
-//   mode: 'history',
-//   base: process.env.BASE_URL,
-//   routes
-// })
-
-// export default router
+export default router
